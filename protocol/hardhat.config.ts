3fade6ff26615ba0132c352bee14c25b40f3708a
techyNonso <williamikeji@gmail.com>
2024-01-16 23:04:24 +0100

    forms for trade and deposit done

--- protocol/hardhat.config.ts
+++ protocol/hardhat.config.ts
@@ -1,51 +1,59 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
-import 'hardhat-abi-exporter';
-import 'hardhat-contract-sizer';
-import dotenv from 'dotenv';
+import "hardhat-abi-exporter";
+import "hardhat-contract-sizer";
+import dotenv from "dotenv";
 
-dotenv.config()
-
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: {
-	compilers: [
-		{
-			version: "0.8.20",
-			settings: {
-				optimizer: {
-					enabled: true,
-					runs: 200,
-				},
-			},
-		},
-	]
+    compilers: [
+      {
+        version: "0.8.20",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
   },
+
   abiExporter: [
-		{
-			path: '../client/src/abi',
-			pretty: false,
-			runOnCompile: true,
-      		only: ["PerpTrades", "CollateralBank", "MockERC20.sol"]
-		}
-	],
-	contractSizer: {
-		alphaSort: true,
-		disambiguatePaths: false,
-		runOnCompile: true,
-		strict: true,
-		only: [ "PerpTrades", "CollateralBank"]
-	},
-	// networks: {
-	// 	mumbai: {
-	// 		url: 'https://polygon-mumbai.g.alchemy.com/v2/1yHVzG9cEm8g0IJKQA0VO-nczdGW4NgO',
-	// 		accounts: [ PRIVATE_KEY ]
-	// 	},
-	// 	fuji: {
-	// 		url: 'https://aged-wider-needle.avalanche-testnet.quiknode.pro/d0313dfde220dd7912c814a6fa97a0620e95d924/ext/bc/C/rpc/',
-	// 		accounts: [ PRIVATE_KEY ]
-	// 	},
-	// },
+    {
+      path: "../client/src/abi",
+      pretty: false,
+      runOnCompile: true,
+      only: ["PerpTrades", "CollateralBank", "MockERC20.sol"],
+    },
+  ],
+  contractSizer: {
+    alphaSort: true,
+    disambiguatePaths: false,
+    runOnCompile: true,
+    strict: true,
+    only: ["PerpTrades", "CollateralBank"],
+  },
+
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337, // Customize the chain ID here
+    },
+  },
+
+  // networks: {
+  // 	mumbai: {
+  // 		url: 'https://polygon-mumbai.g.alchemy.com/v2/1yHVzG9cEm8g0IJKQA0VO-nczdGW4NgO',
+  // 		accounts: [ PRIVATE_KEY ]
+  // 	},
+  // 	fuji: {
+  // 		url: 'https://aged-wider-needle.avalanche-testnet.quiknode.pro/d0313dfde220dd7912c814a6fa97a0620e95d924/ext/bc/C/rpc/',
+  // 		accounts: [ PRIVATE_KEY ]
+  // 	},
+  // },
 };
 
 export default config;
