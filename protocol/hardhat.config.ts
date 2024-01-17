cc912d6df7c93d628894d695b190f7128e587f6f
Joshua Evuetapha <Evuetaphajoshua@gmail.com>
2024-01-17 17:20:04 +0100

    fixes: bug fixes

--- protocol/hardhat.config.ts
+++ protocol/hardhat.config.ts
@@ -6,6 +6,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const PRIVATE_KEY  = String(process.env.PRIVATE_KEY)
+
 const config: HardhatUserConfig = {
   solidity: {
     compilers: [
@@ -42,18 +44,11 @@ const config: HardhatUserConfig = {
     hardhat: {
       chainId: 1337, // Customize the chain ID here
     },
+    sepolia: {
+  		url: 'https://sepolia.drpc.org',
+  		accounts: [ PRIVATE_KEY ]
+  	},
   },
-
-  // networks: {
-  // 	mumbai: {
-  // 		url: 'https://polygon-mumbai.g.alchemy.com/v2/1yHVzG9cEm8g0IJKQA0VO-nczdGW4NgO',
-  // 		accounts: [ PRIVATE_KEY ]
-  // 	},
-  // 	fuji: {
-  // 		url: 'https://aged-wider-needle.avalanche-testnet.quiknode.pro/d0313dfde220dd7912c814a6fa97a0620e95d924/ext/bc/C/rpc/',
-  // 		accounts: [ PRIVATE_KEY ]
-  // 	},
-  // },
 };
 
 export default config;
