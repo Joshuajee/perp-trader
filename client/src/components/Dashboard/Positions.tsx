fc5a7b60000cd90ebc4da667fe5ed85a8d9a5740
techyNonso <williamikeji@gmail.com>
2024-01-19 00:06:29 +0100

    position closing added

--- client/src/components/Dashboard/Positions.tsx
+++ client/src/components/Dashboard/Positions.tsx
@@ -2,9 +2,11 @@ import { TbArrowBigDownLinesFilled, TbArrowBigUpLinesFilled } from 'react-icons/
 import useCurrentChainId from "@hooks/useCurrentChainId"
 import { useAccount, useContractRead } from "wagmi";
 import perpAbi from "@abis/contracts/PerpTrades.sol/PerpTrades.json"
+import { publicClient, walletClient } from '@utils/helpers';
 
 
 interface IPosition {
+    positionId: bigint,
     pnl: bigint,
     position: {
         isLong: boolean,
@@ -37,6 +39,23 @@ export function Positions() {
     })
 
 
+
+    const closePosition = async (id: number) => {
+        console.log(id)
+
+        const { request } = await publicClient.simulateContract({
+            address: `0x${import.meta.env.VITE_PERP_TRADER_ADDRESS.substring(2)}`,
+            abi: perpAbi,
+            functionName: 'closePosition',
+            args: [id],
+            account: address,
+        })
+
+        //@ts-ignore
+        const hash = await walletClient.writeContract(request)
+        console.log(hash, "transaction completed")
+    }
+
     return (
         <div className="bg-primary_4 rounded-md py-3 h-[100vh] overflow-auto">
             <div className="flex items-center border-b border-primary_2 h-10">
@@ -55,7 +74,7 @@ export function Positions() {
                         <div className={`w-[16.6%]  h-full font-semibold text-sm flex items-center justify-center ${Number(position.pnl) > 0 ? "text-green-600" : "text-red-600"} `}>{Number(position.pnl)}</div>
                         <div className="w-[16.6%]  h-full font-semibold text-sm flex items-center justify-center"><button className=" w-fit px-3 py-2 flex items-center text-xs  hover:bg-red-600 hover:text-white  ">Decrease</button></div>
                         <div className="w-[16.6%]  h-full font-semibold text-sm flex items-center justify-center"><button className=" w-fit px-3 py-2 flex items-center text-xs  hover:bg-green-600 hover:text-white  ">Increase</button></div>
-                        <div className="w-[16.6%]  h-full font-semibold text-sm flex items-center justify-center"><button className=" w-fit px-3 py-2 flex items-center text-xs  hover:bg-blue-600 hover:text-white  ">Close</button></div>
+                        <div className="w-[16.6%]  h-full font-semibold text-sm flex items-center justify-center"><button onClick={() => closePosition(Number(position.positionId))} className=" w-fit px-3 py-2 flex items-center text-xs  hover:bg-blue-600 hover:text-white  ">Close</button></div>
 
                     </div>
                 ))}
