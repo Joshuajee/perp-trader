5ebd613ae75e8fa3f806d807c594cfff28fd72e0
techyNonso <williamikeji@gmail.com>
2024-01-19 19:12:12 +0100

    favico added

--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -10,7 +10,7 @@ const Layout = () => {
         <>
             <nav className="h-16 px-5 flex justify-between border-b border-primary_4">
                 <div className="w-[70%] h-full flex gap-7 items-center">
-                    <img src={logo} alt="logo" className="w-24 pt-2" />
+                    <Link to="/"><h1 className="text-white font-semibold">PERP-TRADER</h1></Link>
                     <ul className="flex gap-3 h-full  items-center">
                         <Link to="/"><li className={`px-4 py-2  text-sm font-semibold hover:text-white cursor-pointer flex items-center ${(location.pathname == "/") ? "text-white" : ""}`}>Dashboard</li></Link>
                         <Link to="/trades"><li className={`px-4 py-2  text-sm font-semibold hover:text-white cursor-pointer flex items-center ${(location.pathname == "/trades") ? "text-white" : ""}`}>Trades</li></Link>
