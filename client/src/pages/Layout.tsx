0a0ea4298b7e404ae991a341f174ef7d707d5200
techyNonso <williamikeji@gmail.com>
2024-01-20 11:54:04 +0100

    logo added

--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -1,5 +1,5 @@
 import { Outlet, Link, useLocation } from "react-router-dom";
-
+import logo from "@assets/images/logo.png"
 import { ConnectKitButton } from "connectkit";
 
 const Layout = () => {
@@ -9,7 +9,9 @@ const Layout = () => {
         <>
             <nav className="h-16 px-5 flex justify-between border-b border-primary_4">
                 <div className="w-[70%] h-full flex gap-7 items-center">
-                    <Link to="/"><h1 className="text-white font-semibold">PERP-TRADER</h1></Link>
+                    <Link to="/">
+                        <img src={logo} className="w-32" />
+                    </Link>
                     <ul className="flex gap-3 h-full  items-center">
                         <Link to="/"><li className={`px-4 py-2  text-sm font-semibold hover:text-white cursor-pointer flex items-center ${(location.pathname == "/") ? "text-white" : ""}`}>Dashboard</li></Link>
                         <Link to="/trades"><li className={`px-4 py-2  text-sm font-semibold hover:text-white cursor-pointer flex items-center ${(location.pathname == "/trades") ? "text-white" : ""}`}>Trades</li></Link>
